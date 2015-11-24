2d500b5b546be9df560a834dde93ebed690a2e3c
Jordan Hovdebo <jordan.hovdebo@gmail.com>
2015-11-23 21:56:50 -0600

    Fixing the build

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    std::out << "Some message" << std::endl;
+    std::cout << "Some message" << std::endl;
     std::cout << " line 2" << std::endl;
     std::cout << " line 3" << std::endl;
     return 1;
